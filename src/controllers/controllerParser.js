2bde25943bad34d9c22c3e2811716200c77b87c5
Ana Czarnitzki <czarniana@gmail.com>
2017-09-14 10:45:57 -0300

    Arreglo códigos de error del parser, lo vuelvo más genérico

--- src/controllers/controllerParser.js
+++ src/controllers/controllerParser.js
@@ -3,13 +3,15 @@ function jsonParser(j) {
   return JSON.parse(str);
 }
 
-function errorParser(r, response){
+//----------------------->Basic Parsers<-----------------------//
+
+function reducedParser(r, response){
   return response.status(r.status).json({code: r.status, message: r.data});
 }
 
-function basicParser(r, response, tag){
+function extendedParser(r, response, tag, ok_status){
   if (!r.success){
-    return errorParser(r, response);
+    return reducedParser(r, response);
   }
   var jObj = {metadada: { //TODO: complete
     "count": 0,
@@ -21,22 +23,94 @@ function basicParser(r, response, tag){
     "version": "string"
   }};
   jObj[tag] = r.data;
-  return response.status(r.status).json(jObj);
+  return response.status(ok_status).json(jObj);
 }
 
-function parserUsers(r, response){
-  basicParser(r, response, "users");
+//----------------------------------------------//
+
+//----------------------->Parser Users<-----------------------// //TODO: rethink code
+
+function parserUsers(r, response, ok_status){
+  extendedParser(r, response, "users", ok_status);
 }
 
-function parserBusinessUsers(r, response){
-  basicParser(r, response, "businessUsers");
+function parserUsersPost(r, response){
+  parserUsers(r, response, 201);
 }
 
-function parserServers(r, response){
-  basicParser(r, response, "servers");
+function parserUsersPut(r, response){
+  parserUsers(r, response, 200);
 }
 
-module.exports.jsonParser = jsonParser;
-module.exports.parserUsers = parserUsers;
-module.exports.parserBusinessUsers = parserBusinessUsers;
-module.exports.parserServers = parserServers;
+function parserUsersDelete(r, response){
+  parserUsers(r, response, 204);
+}
+
+function parserUsersGet(r, response){
+  parserUsers(r, response, 200);
+}
+
+//----------------------------------------------//
+
+//----------------------->Parser Business Users<-----------------------//
+function parserBusinessUsers(r, response, ok_status){
+  extendedParser(r, response, "businessUsers", ok_status);
+}
+
+function parserBusinessUsersPost(r, response){
+  parserBusinessUsers(r, response, 201);
+}
+
+function parserBusinessUsersPut(r, response){
+  parserBusinessUsers(r, response, 200);
+}
+
+function parserBusinessUsersDelete(r, response){
+  parserBusinessUsers(r, response, 204);
+}
+
+function parserBusinessUsersGet(r, response){
+  parserBusinessUsers(r, response, 200);
+}
+
+//----------------------------------------------//
+
+//----------------------->Parser Servers<-----------------------//
+function parserServers(r, response, ok_status){
+  extendedParser(r, response, "servers", ok_status);
+}
+
+function parserServersPost(r, response){
+  parserServers(r, response, 201);
+}
+
+function parserServersPut(r, response){
+  parserServers(r, response, 200);
+}
+
+function parserServersDelete(r, response){
+  parserServers(r, response, 204);
+}
+
+function parserServersGet(r, response){
+  parserServers(r, response, 200);
+}
+
+//----------------------------------------------//
+
+module.exports = {
+    parserServersGet : parserServersGet,
+    parserServersDelete : parserServersDelete,
+    parserServersPut : parserServersPut,
+    parserServersPost : parserServersPost,
+
+    parserUsersGet : parserUsersGet,
+    parserUsersDelete : parserUsersDelete,
+    parserUsersPut : parserUsersPut,
+    parserUsersPost : parserUsersPost,
+
+    parserBusinessUsersGet : parserBusinessUsersGet,
+    parserBusinessUsersDelete : parserBusinessUsersDelete,
+    parserBusinessUsersPut : parserBusinessUsersPut,
+    parserBusinessUsersPost : parserBusinessUsersPost,
+};
