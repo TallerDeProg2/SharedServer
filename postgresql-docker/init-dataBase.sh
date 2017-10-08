41a98111944dc011a1a40ffab5c0fa6c2dfb7817
Ana Czarnitzki <czarniana@gmail.com>
2017-10-08 18:40:16 -0300

    arreglo docker (?)

--- postgresql-docker/init-dataBase.sh
+++ postgresql-docker/init-dataBase.sh
@@ -4,8 +4,6 @@ set -e
 time_exp=$(date +"%Y-%m-%d %H:%M:%S %Z" -d "+1 day")
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE docker;
-    GRANT ALL PRIVILEGES ON DATABASE docker TO postgres;
     CREATE TABLE srvusers (id text, _ref text, rol text, token text, tokenexp timestamp, data jsonb);
     INSERT INTO srvusers(id, _ref, rol, token, tokenexp, data) VALUES ('1', 'rtfhgf', 'server', 'token', '$time_exp',
       '{"createdBy" : "docker","createdTime" : "$time_exp", "name" : "servercito", "lastConnection" : "$time_exp" }');
