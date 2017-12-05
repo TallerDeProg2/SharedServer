01751a62321d13ee5e867b21ad6253171ec3b1f8
Ana Czarnitzki <czarniana@gmail.com>
2017-12-05 17:17:30 -0300

    fix (2)

--- src/routes/router.js
+++ src/routes/router.js
@@ -7,8 +7,8 @@ var express = require('express');
   */
 var router = express.Router();
 
-router.get('*', function(req, res) {
-	res.sendfile('../webapp/index.html');
+router.get('/index', function(req, res) {
+	res.sendFile('index.html', { root: './src/webapp/' });
 });
 
 module.exports = router;
