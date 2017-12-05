6029eb883bb2d6d883a2ea9b0e5f13cfddd514c4
Ana Czarnitzki <aczarnitzki@medallia.com>
2017-12-05 14:28:09 -0300

    Front-end WIP

--- src/routes/router.js
+++ src/routes/router.js
@@ -7,4 +7,8 @@ var express = require('express');
   */
 var router = express.Router();
 
+router.get('*', function(req, res) {
+	res.sendfile('../webapp/index.html');
+});
+
 module.exports = router;
