2ebfd677e046e896c3dc81657de5e44e77252441
Ana Czarnitzki <aczarnitzki@medallia.com>
2017-12-09 19:41:51 -0300

    WIP token not expired

--- appclient-src/src/app/services/auth.service.ts
+++ appclient-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -26,6 +27,10 @@ export class AuthService {
     this.authToken = token;
     this.user = user;
   }
+
+  loggedIn(){
+    return tokenNotExpired();
+}
 
   logout(){
     this.authToken = null;
